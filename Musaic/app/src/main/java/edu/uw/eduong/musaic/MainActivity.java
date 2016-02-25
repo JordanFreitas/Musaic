02f8409ed0f55a8a274fdfa744fb40e39ec2d243
luludust <elizabethduong96@gmail.com>
2016-02-24 17:48:34 -0800

    Add app

---
+++ Musaic/app/src/main/java/edu/uw/eduong/musaic/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.uw.eduong.musaic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+// Displays a list of music on your phone which can be clicked on to play
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
