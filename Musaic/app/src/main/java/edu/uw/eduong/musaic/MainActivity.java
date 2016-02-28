a73852927d4872ebe4d2d8efca306bf1e403b26a
luludust <elizabethduong96@gmail.com>
2016-02-28 02:51:27 -0800

    This should actually work i think

--- Musaic/app/src/main/java/edu/uw/eduong/musaic/MainActivity.java
+++ Musaic/app/src/main/java/edu/uw/eduong/musaic/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,7 +20,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 // Displays a list of music on your phone which can be clicked on to play
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
     private static final String TAG = "Music";
     private ArrayAdapter<Song> adapter;   // holds the list of songs to display
@@ -42,41 +44,47 @@ public class MainActivity extends AppCompatActivity {
     // Get list of songs on device
     public void getSongs() {
         if (isExternalStorageReadable()) {
-            if (ContextCompat.checkSelfPermission(this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-
-                // from http://code.tutsplus.com/tutorials/create-a-music-player-on-android-project-setup--mobile-22764
-                ContentResolver musicResolver = getContentResolver();
-                Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
-
-                if (musicCursor != null && musicCursor.moveToFirst()) {
-                    //get columns
-                    int titleColumn = musicCursor.getColumnIndex
-                            (android.provider.MediaStore.Audio.Media.TITLE);
-                    int idColumn = musicCursor.getColumnIndex
-                            (android.provider.MediaStore.Audio.Media._ID);
-                    int artistColumn = musicCursor.getColumnIndex
-                            (android.provider.MediaStore.Audio.Media.ARTIST);
-                    //add songs to list
-                    do {
-                        long id = musicCursor.getLong(idColumn);
-                        String title = musicCursor.getString(titleColumn);
-                        String artist = musicCursor.getString(artistColumn);
-                        Log.v(TAG, title);
-                        adapter.add(new Song(id, title, artist));
-                    }
-                    while (musicCursor.moveToNext());
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    displayList();
+                } else {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 255);
                 }
             } else {
-                Toast.makeText(getApplicationContext(),
-                        "Cannot access external storage", Toast.LENGTH_LONG).show();
-                Log.v(TAG, "Cannot access external storage");
+                displayList();
             }
         } else {
             Toast.makeText(getApplicationContext(),
                     "Cannot read external storage", Toast.LENGTH_LONG).show();
             Log.v(TAG, "Cannot read external storage");
+        }
+    }
+
+    // Display list
+    public void displayList() {
+        // from http://code.tutsplus.com/tutorials/create-a-music-player-on-android-project-setup--mobile-22764
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if (musicCursor != null && musicCursor.moveToFirst()) {
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long id = musicCursor.getLong(idColumn);
+                String title = musicCursor.getString(titleColumn);
+                String artist = musicCursor.getString(artistColumn);
+                Log.v(TAG, title);
+                adapter.add(new Song(id, title, artist));
+            }
+            while (musicCursor.moveToNext());
         }
     }
 
@@ -89,5 +97,18 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
+    }
+
+    // http://stackoverflow.com/questions/33162152/storage-permission-error-in-marshmallow/33162201
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+            displayList();
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    "Cannot display songs, please enable permission", Toast.LENGTH_LONG).show();
+            Log.v(TAG, "Cannot read external storage, permission denied");
+        }
     }
 }
